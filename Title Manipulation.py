26a48553675c872cda86bb2c6b36c2b655d55b75
rahmanseo <touchrahman@gmail.com>
2022-12-09 00:15:44 +0800

    Upload practice file

---
+++ Title Manipulation.py
@@ -0,0 +1,5 @@
+str = "Mahbobur Rahman"
+print("Title: ",str.title()) #first word capitalized of each sentences
+print("Lower: ",str.lower()) #every word small of each sentences
+print("Upper: ",str.upper()) #every word capitalized of each sentences
+print("Swacase: ",str.swapcase()) #first word small of each sentences
